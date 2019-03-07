9e0932e8fe8a6629393e0d2afd53d2c1db0b4bf3
yuvaraj kadam <yuvarajkadam@macusers-iMac-2.local>
2019-03-07 14:28:30 +0000

    class component changed to function in Home.jsx

--- ncnewsfe2/src/components/Home.jsx
+++ ncnewsfe2/src/components/Home.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import './Home.css';
 
-class Home extends React.Component {
-  render() {
-    const { username } = this.props;
-    //console.log(this.props);
+const Home = props => {
+  const { username } = props;
+  //console.log(this.props);
 
-    return (
-      <div className="container">
-        <div className="grid-row">
-          <h1>Welcome to NC News </h1>
+  return (
+    <div className="container">
+      <div className="grid-row">
+        <h1>Welcome to NC News </h1>
 
-          <h2>{`${username}`} </h2>
-        </div>
+        <h2>{`${username}`} </h2>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Home;
